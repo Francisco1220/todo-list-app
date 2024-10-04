4d5989d52644a54ead9ac23239298f23e8163ce6
Francisco <franciscocolombocouste@gmail.com>
2024-10-04 12:17:46 +0200

    Restructure code for submitting new task

--- src/dom.js
+++ src/dom.js
@@ -3,40 +3,32 @@
 export const newTask = document.querySelector("li:nth-child(6)");
 const dialog = document.querySelector("#task-form");
 const submitTaskBtn = document.querySelector("#create-task");
+import {checkFormComplete} from "./index.js"
 
 newTask.addEventListener("click", () => {
     // opens modal
     dialog.showModal();
 })
 
-function checkFormComplete () {
-    let titleVal = document.getElementById("title").value;
-    let priorityVal = document.getElementById("priority").value;
-    if (title === "" || priority === "default") {
-        alert("Please make sure you have filled out 'Title' and 'Priority'");
-    } else {
-        dialog.close();
-    }
-    return {titleVal, priorityVal}
-}
-
-
 submitTaskBtn.addEventListener("click", (e) => {
     // closes modal
     e.preventDefault();
     // client-side validation (check that inputs are completed)
+    getTaskInput();
     checkFormComplete();
-
-    // Get values from each input
-    let titleVal = document.getElementById("title").value;
-    let descriptionVal = document.getElementById("description").value;
-    let dueDate = document.getElementById("date").value;
-    let priorityVal = document.getElementById("priority").value;
-    let project = document.getElementById("project").value;
-    // Return as object the values of each input
-    return {titleVal, descriptionVal, dueDate, priorityVal, project}
+    const {closeDialog, alertMessage} = checkFormComplete();
+    if (closeDialog === false) {
+        alert(alertMessage);
+    } else {
+        dialog.close();
+    }
 })
 
 export function getTaskInput () {
-    console.log(titleVal);
+    let titleVal = document.getElementById("title").value;
+    let descriptionVal = document.getElementById("description").value;
+    let dueDateVal = document.getElementById("date").value;
+    let priorityVal = document.getElementById("priority").value;
+    let projectVal = document.getElementById("project").value
+    return {titleVal, descriptionVal, dueDateVal, priorityVal, projectVal}
 }
--- src/index.js
+++ src/index.js
@@ -1,4 +1,19 @@
 // Imported files
-import "./style.css"
+import "./style.css";
+import {newTask, getTaskInput} from "./dom.js";
 
-import {newTask} from "./sidebar.js"
+// Application Logic goes here (creating new todos, setting todos as complete, changing todo priority)
+
+export function checkFormComplete (firstVal, secondVal) {
+    const {titleVal, priorityVal} = getTaskInput();
+    firstVal = titleVal;
+    secondVal = priorityVal;
+    let closeDialog = false;
+    let alertMessage = "Please make sure you have filled out 'Title' and 'Priority'";
+    if (firstVal === "" || secondVal === "default") {
+        closeDialog = false;
+    } else {
+        closeDialog = true;
+    }
+    return {closeDialog, alertMessage}
+}
