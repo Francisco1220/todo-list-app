545351e7719374743d9e6b7b3c4b554d4cbee274
Francisco <franciscocolombocouste@gmail.com>
2024-10-04 16:07:35 +0200

    Create task card DOM elements on each submission

--- src/dom.js
+++ src/dom.js
@@ -3,7 +3,12 @@
 export const newTask = document.querySelector("li:nth-child(6)");
 const dialog = document.querySelector("#task-form");
 const submitTaskBtn = document.querySelector("#create-task");
-import {checkFormComplete, createTask} from "./index.js"
+import {checkFormComplete, createTask, taskList} from "./index.js"
+
+// import (default) image SVGs
+
+import editSVG from "./assets/icons/edit-pen.svg";
+import deleteSVG from "./assets/icons/delete.svg";
 
 newTask.addEventListener("click", () => {
     // opens modal
@@ -34,3 +39,39 @@ export function getTaskInput () {
     return {titleVal, descriptionVal, dueDateVal, priorityVal, projectVal}
 }
 
+// Create DOM for each task card
+function createCards () {
+    const divContainer = document.querySelector(".tasks-container");
+
+    const cardDiv = document.createElement("div");
+    cardDiv.setAttribute("class", "task-cards");
+    divContainer.appendChild(cardDiv);
+    
+    const checkDiv = document.createElement("div");
+    checkDiv.setAttribute("class", "circle-check");
+    cardDiv.appendChild(checkDiv);
+
+    const titleDiv = document.createElement("p");
+    titleDiv.setAttribute("class", "task-title");
+    checkDiv.insertAdjacentElement("afterend", titleDiv);
+
+    const cardOptionsDiv = document.createElement("div");
+    cardOptionsDiv.setAttribute("class", "card-interact");
+    titleDiv.insertAdjacentElement("afterend", cardOptionsDiv);
+
+    const descriptionBtn = document.createElement("button");
+    descriptionBtn.innerHTML = "description";
+    cardOptionsDiv.appendChild(descriptionBtn);
+
+    const editImage = document.createElement("img");
+    editImage.src = editSVG;
+    descriptionBtn.insertAdjacentElement("afterend", editImage);
+
+    const deleteImage = document.createElement("img");
+    deleteImage.src = deleteSVG;
+    descriptionBtn.insertAdjacentElement("afterend", deleteImage);
+
+    const date = document.createElement("p");
+    date.setAttribute("class", "date");
+    cardDiv.appendChild(date);
+}
